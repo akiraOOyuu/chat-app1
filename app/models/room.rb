70aaa46da389f87a0f336988a351a6df8b497011
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-05-05 19:41:18 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,5 @@ class Room < ApplicationRecord
   validates :name, presence: true          #バリデーション ルーム名が存在してる場合のみＯＫ
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :messages
 end
