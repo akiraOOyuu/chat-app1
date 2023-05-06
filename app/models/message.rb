040a2b81a37f1dccbd3ae8b2c382dc1c09514522
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-05-06 14:50:08 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,9 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image                     #イメージファイル紐づけ
-  validates :content, presence: true          #バリデーション
+  validates :content, presence: true, unless: :was_attached?          #バリデーション
+
+  def was_attached?
+    self.image.attached?
+  end
 end
